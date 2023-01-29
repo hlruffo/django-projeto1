26ea703b1bfa1351ce94b3952616a5947436a0fb
Hugo Ledertheil Ruffo <hlruffo@gmail.com>
2023-01-29 16:30:04 -0300

    Final aula 53

--- recipes/models.py
+++ recipes/models.py
@@ -1,7 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
+
 class Recipe(models.Model):
     title = models.CharField(max_length=65),
     description = models.CharField(max_length=165),
@@ -16,4 +22,6 @@ class Recipe(models.Model):
     updated_at = models.DateField(auto_now=True)
     is_published = models.BooleanField(default=False),
     cover = models.ImageField(upload_to="reciés/covers/%Y/%m/%d/"),
-    
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True),
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
